fcd8794f1a2156e4b14f2e15c71bac297d38df95
matthewcarcao <matthew.carcao@greenwoodcollege.com>
2019-11-06 12:14:16 -0500

    Task 5 complete

--- Functions.py
+++ Functions.py
@@ -21,13 +21,24 @@ import random
 #     return text
 # data1=str(input("Write something"))
 # print(data(data1))
+#
+# def lottery(num1,num2,num3):
+#     lot1=random.randint(1,10)
+#     lot2=random.randint(1,10)
+#     lot3=random.randint(1,10)
+#     if num1==lot1 and num2==lot2 and num3==lot3:
+#         return "You won the lottery!"
+#     else:
+#         return "You lost!"
+# print(lottery(1,6,3))
 
-def lottery(num1,num2,num3):
-    lot1=random.randint(1,10)
-    lot2=random.randint(1,10)
-    lot3=random.randint(1,10)
-    if num1==lot1 and num2==lot2 and num3==lot3:
-        return "You won the lottery!"
-    else:
-        return "You lost!"
-print(lottery(1,6,3))
+def task_5(li,num):
+    i=0
+    while len(li)>i:
+        if li[i]>num:
+            del li[i]
+            i=i-1
+        i=i+1
+    return li
+ex_list = [0,1,2,3,4,5,6,7,8,9]
+print (task_5(ex_list,4))
