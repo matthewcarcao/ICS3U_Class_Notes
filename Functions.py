3322031b4dc8fe7911c5ea0e89ec56db2ed40203
matthewcarcao <matthew.carcao@greenwoodcollege.com>
2019-11-06 12:02:32 -0500

    Task 4 complete

--- Functions.py
+++ Functions.py
@@ -10,12 +10,12 @@ import random
 # length=float(input("Low me a length ahlie"))
 # square = area_of_square(length)
 # print(int(square))
-
+#
 # text = str(input("Write something"))
 # def data():
 #     print (text*5)
 # data()
-
+#
 # def data(text):
 #     text=text.upper()
 #     return text
@@ -24,13 +24,10 @@ import random
 
 def lottery(num1,num2,num3):
     lot1=random.randint(1,10)
-    return lot1
     lot2=random.randint(1,10)
-    return lot2
     lot3=random.randint(1,10)
-    return lot3
-guess1=int(input("Pick a number from 1-10"))
-guess2=int(input("Pick another number from 1-10"))
-guess3=int(input("Pick your final number from 1-10"))
-if guess1==lotran1(0) and guess2==lotran2(1) and guess3==lotran3(2):
-    print("You won the lottery!")
+    if num1==lot1 and num2==lot2 and num3==lot3:
+        return "You won the lottery!"
+    else:
+        return "You lost!"
+print(lottery(1,6,3))
